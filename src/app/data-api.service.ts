ea4369768f46b6edca45ed2a83de78aa65306576
julie <jtrianom@correo.udistrital.edu.co>
2021-10-14 23:38:09 -0500

    doublevparners

--- src/app/data-api.service.ts
+++ src/app/data-api.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
 import { User } from './main/user';
+import { arrow } from '@popperjs/core';
 
 
 @Injectable({
@@ -10,7 +11,9 @@ import { User } from './main/user';
 })
 export class DataAPIService {
   private REST_API_SERVER = "https://api.github.com/users";
+  private API_SERVER_SEARCH= "https://api.github.com/search/users?q=";
   public userName: String='';
+  public data =[]
   constructor(private http: HttpClient) { }
   
   getUsers(): Observable<User[]>{
@@ -24,6 +27,18 @@ export class DataAPIService {
       });
       })
     );
+  }
+
+  searchUsers(userName:String):Observable<any[]>{
+    console.log(this.API_SERVER_SEARCH+userName);
+     return this.http.get<any[]>(this.API_SERVER_SEARCH+userName).pipe(map(arr =>{ 
+       var dat= Object.values(arr)[2];
+       console.log(dat);
+      return dat;
+      })
+    );
+    
+    
   }
 
   getUserbyId(id:String){
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -3,6 +3,7 @@ import { User } from './user';
 
 import { DataAPIService } from '../data-api.service';
 import { Router } from '@angular/router';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-main',
@@ -11,11 +12,14 @@ import { Router } from '@angular/router';
 })
 export class MainComponent implements OnInit {
   users: User[] = [];
+  data:any[]= []
+  username = new FormControl('userform', Validators.min(4));
+  
   private iduser:String='';
   constructor(private dataService: DataAPIService, private router: Router) { }
 
   ngOnInit() {
-
+    
     this.dataService.getUsers()
     .subscribe(users => (this.users = users));
   }
@@ -24,6 +28,14 @@ export class MainComponent implements OnInit {
     console.log("funciono", userName);
     this.dataService.userName =  userName;
     this.router.navigate(["/user"]);
+  }
+  searchName(userName:String){
+    if(userName){
+      this.dataService.searchUsers(userName)
+      .subscribe(data => (this.users = data));
+      console.log(this.users);
+    }
+
   }
 
 }
